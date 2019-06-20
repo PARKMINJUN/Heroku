febd81b4122763ec19406b5f193edee7aa3b1181
PARKMINJUN <junee0530@naver.com>
2019-06-20 20:34:56 +0900

    first commit of portfolio proj

--- second/secondproject/urls.py
+++ second/secondproject/urls.py
@@ -1,14 +1,14 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+
 import blog.views
 import portfolio.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', blog.views.home, name='home'),
-    path('blog/<int:blog_id>', blog.views.detail, name="detail"),
-    path('blog/new/', blog.views.new, name='new'),
-    path('blog/create', blog.views.create, name="create"),
+    path('blog/',include("blog.urls")),
     path('portfolio/', portfolio.views.portfolio, name="portfolio"),
 ]
